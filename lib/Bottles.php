26955c6273c5cfe61808e280d0ae6fe7cdbbc60d
David Stockton <dave@davidstockton.com>
2018-07-14 13:26:05 -0600

    Superfluously General example

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -2,24 +2,84 @@
 
 class Bottles
 {
+    public function noMore()
+    {
+        return "No more bottles of beer on the wall, " .
+            "no more bottles of beer.\n" .
+            "Go to the store and buy some more, " .
+            "99 bottles of beer on the wall.\n";
+    }
+
+    public function lastOne()
+    {
+        return "1 bottle of beer on the wall, " .
+            "1 bottle of beer.\n" .
+            "Take it down and pass it around, " .
+            "no more bottles of beer on the wall.\n";
+    }
+
+    public function penultimate()
+    {
+        return "2 bottles of beer on the wall, " .
+            "2 bottles of beer.\n" .
+            "Take one down and pass it around, " .
+            "1 bottle of beer on the wall.\n";
+    }
+
+    public function default($number)
+    {
+        return "{$number} bottles of beer on the wall, " .
+            "{$number} bottles of beer.\n" .
+            "Take one down and pass it around, " .
+            ($number-1) . " bottles of beer on the wall.\n";
+    }
+
     public function song()
     {
         return $this->verses(99, 0);
     }
 
-    public function verses($hi, $lo)
+    public function verses($finish, $start)
     {
-        return implode("\n", array_map(function ($n) {
-            return $this->verse($n);
-        }, range($hi, $lo)));
+        return implode(
+            "\n",
+            array_map(function ($verseNumber) {
+                return $this->verse($verseNumber);
+            },
+            range($finish, $start, -1)
+            )
+        );
     }
 
-    public function verse($n)
+    public function verse($number)
     {
-        return
-            ($n == 0 ? "No more" : $n)." bottle".($n != 1 ? 's' : '')." of beer on the wall, ".
-            ($n == 0 ? "no more" : $n)." bottle".($n != 1 ? 's' : '')." of beer.\n".
-            ($n > 0 ? "Take ".($n > 1 ? 'one' : 'it')." down and pass it around, " : "Go to the store and buy some more, ").
-            ($n - 1 < 0 ? 99 : ($n - 1 == 0 ? 'no more' : $n - 1))." bottle".($n - 1 != 1 ? 's' : '')." of beer on the wall.\n";
+        return $this->verseFor($number)->text();
+    }
+
+    private function verseFor($number)
+    {
+        switch ($number) {
+            case 0: return new Verse($number, [$this, 'noMore']);
+            case 1: return new Verse($number, [$this, 'lastOne']);
+            case 2: return new Verse($number, [$this, 'penultimate']);
+            default: return new Verse($number, [$this, 'default']);
+        }
+    }
+}
+
+class Verse
+{
+    protected $number;
+    private $lyrics;
+
+    public function __construct($number, $lyrics)
+    {
+        $this->number = $number;
+        $this->lyrics = $lyrics;
+    }
+
+    public function text()
+    {
+        return ($this->lyrics)($this->number);
     }
 }
