ca742628de6bebfc7c16913b84bac3cfbb4d88b0
David Stockton <dave@davidstockton.com>
2018-07-18 22:22:58 -0600

    Update example-superfluously-general with Dann's gist

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -2,34 +2,37 @@
 
 class Bottles
 {
-    public function lambdas()
+    private $noMore;
+    private $lastOne;
+    private $penultimate;
+    private $default;
+
+    public function __construct()
     {
-        return [
-            'NoMore' => function ($verse) {
-                return "No more bottles of beer on the wall, " .
-                    "no more bottles of beer.\n" .
-                    "Go to the store and buy some more, " .
-                    "99 bottles of beer on the wall.\n";
-            },
-            'LastOne' => function ($verse) {
-                return "1 bottle of beer on the wall, " .
-                    "1 bottle of beer.\n" .
-                    "Take it down and pass it around, " .
-                    "no more bottles of beer on the wall.\n";
-            },
-            'Penultimate' => function ($verse) {
-                return "2 bottles of beer on the wall, " .
-                    "2 bottles of beer.\n" .
-                    "Take one down and pass it around, " .
-                    "1 bottle of beer on the wall.\n";
-            },
-            'Default' => function ($verse) {
-                return "{$verse->number()} bottles of beer on the wall, " .
-                    "{$verse->number()} bottles of beer.\n" .
-                    "Take one down and pass it around, " .
-                    ($verse->number() - 1) . " bottles of beer on the wall.\n";
-            }
-        ];
+        $this->noMore = function ($verse) {
+            return "No more bottles of beer on the wall, " .
+                "no more bottles of beer.\n" .
+                "Go to the store and buy some more, " .
+                "99 bottles of beer on the wall.\n";
+        };
+        $this->lastOne = function ($verse) {
+            return "1 bottle of beer on the wall, " .
+                "1 bottle of beer.\n" .
+                "Take it down and pass it around, " .
+                "no more bottles of beer on the wall.\n";
+        };
+        $this->penultimate = function ($verse) {
+            return "2 bottles of beer on the wall, " .
+                "2 bottles of beer.\n" .
+                "Take one down and pass it around, " .
+                "1 bottle of beer on the wall.\n";
+        };
+        $this->default = function ($verse) {
+            return "{$verse->number()} bottles of beer on the wall, " .
+                "{$verse->number()} bottles of beer.\n" .
+                "Take one down and pass it around, " .
+                ($verse->number() - 1) . " bottles of beer on the wall.\n";
+        };
     }
 
     public function song()
@@ -41,10 +44,11 @@ class Bottles
     {
         return implode(
             "\n",
-            array_map(function ($verseNumber) {
-                return $this->verse($verseNumber);
-            },
-            range($finish, $start, -1)
+            array_map(
+                function ($verseNumber) {
+                    return $this->verse($verseNumber);
+                },
+                range($finish, $start, -1)
             )
         );
     }
@@ -57,10 +61,10 @@ class Bottles
     private function verseFor($number)
     {
         switch ($number) {
-            case 0:  return new Verse($number, $this->lambdas()['NoMore']);
-            case 1:  return new Verse($number, $this->lambdas()['LastOne']);
-            case 2:  return new Verse($number, $this->lambdas()['Penultimate']);
-            default: return new Verse($number, $this->lambdas()['Default']);
+            case 0:  return new Verse($number, $this->noMore);
+            case 1:  return new Verse($number, $this->lastOne);
+            case 2:  return new Verse($number, $this->penultimate);
+            default: return new Verse($number, $this->default);
         }
     }
 }
