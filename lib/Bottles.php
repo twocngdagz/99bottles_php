452335e224e1bd8be3412429178d2fcf87c35b04
David Stockton <dave@davidstockton.com>
2018-07-21 18:18:39 -0600

    Make the second test pass

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -2,11 +2,18 @@
 
 class Bottles
 {
-    public function verse($_)
+    public function verse($number)
     {
-        return "99 bottles of beer on the wall, " .
-            "99 bottles of beer.\n" .
-            "Take one down and pass it around, " .
-            "98 bottles of beer on the wall.\n";
+        if ($number === 99) {
+            return "99 bottles of beer on the wall, " .
+                "99 bottles of beer.\n" .
+                "Take one down and pass it around, " .
+                "98 bottles of beer on the wall.\n";
+        } else {
+            return "3 bottles of beer on the wall, " .
+                "3 bottles of beer.\n" .
+                "Take one down and pass it around, " .
+                "2 bottles of beer on the wall.\n";
+        }
     }
 }
