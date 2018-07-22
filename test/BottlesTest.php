abe39cc7a379147992d556b29455165d5e32dd35
David Stockton <dave@davidstockton.com>
2018-07-21 19:05:03 -0600

    Add test for verse 2

--- test/BottlesTest.php
+++ test/BottlesTest.php
@@ -23,4 +23,14 @@ class BottlesTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals($expected, (new Bottles())->verse(3));
     }
+
+    public function test_verse_2()
+    {
+        $expected = "2 bottles of beer on the wall, " .
+            "2 bottles of beer.\n" .
+            "Take one down and pass it around, " .
+            "1 bottle of beer on the wall.\n";
+
+        $this->assertEquals($expected, (new Bottles())->verse(2));
+    }
 }
