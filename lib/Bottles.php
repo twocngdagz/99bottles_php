6ea68a6eb43ff4a8ddb71d51af778ef2f9d8f819
David Stockton <dave@davidstockton.com>
2018-07-21 18:45:48 -0600

    Generalize the verse method

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -4,15 +4,9 @@ class Bottles
 {
     public function verse($number)
     {
-        if ($number === 99) {
-            $n = 99;
-        } else {
-            $n = 3;
-        }
-
-        return "{$n} bottles of beer on the wall, " .
-            "{$n} bottles of beer.\n" .
+        return "{$number} bottles of beer on the wall, " .
+            "{$number} bottles of beer.\n" .
             "Take one down and pass it around, " .
-            ($n - 1) . " bottles of beer on the wall.\n";
+            ($number - 1) . " bottles of beer on the wall.\n";
     }
 }
