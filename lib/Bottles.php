1ccfc4737b976f37a0ce4b281383b3a73709fda4
Dann Stockton <dannstockton@gmail.com>
2018-07-26 20:25:04 -0600

    - This will be unearth-01

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -34,20 +34,10 @@ class Bottles
                     "Take it down and pass it around, " .
                     "no more bottles of beer on the wall.\n";
             case 2:
-                return "2 bottles of beer on the wall, " .
-                    "2 bottles of beer.\n" .
+                return "{$number} bottles of beer on the wall, " .
+                    "{$number} bottles of beer.\n" .
                     "Take one down and pass it around, " .
-                    "1 bottle of beer on the wall.\n";
-            case 6:
-                return "1 six-pack of beer on the wall, " .
-                    "1 six-pack of beer.\n" .
-                    "Take one down and pass it around, " .
-                    "5 bottles of beer on the wall.\n";
-            case 7:
-                return "7 bottles of beer on the wall, " .
-                    "7 bottles of beer.\n" .
-                    "Take one down and pass it around, " .
-                    "1 six-pack of beer on the wall.\n";
+                    ($number - 1) . " bottle of beer on the wall.\n";
             default:
                 return "{$number} bottles of beer on the wall, " .
                     "{$number} bottles of beer.\n" .
