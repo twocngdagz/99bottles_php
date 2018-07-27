b7adfcb55778b348aac246fe4568422545579105
Dann Stockton <dannstockton@gmail.com>
2018-07-26 20:24:14 -0600

    - This will be unearth-00

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -7,46 +7,42 @@ class Bottles
         return $this->verses(99, 0);
     }
 
-    public function verses($start, $ending)
+    public function verses($starting, $ending)
     {
-        return implode("\n", array_map(function ($index) {
-            return $this->verse($index);
-        }, range($start, $ending)));
+        return implode(
+            "\n",
+            array_map(
+                function ($number) {
+                    return $this->verse($number);
+                },
+                range($starting, $ending, -1)
+            )
+        );
     }
 
     public function verse($number)
     {
         switch ($number) {
             case 0:
-                return
-                    "No more bottles of beer on the wall, ".
-                    "no more bottles of beer.\n".
-                    "Go to the store and buy some more, ".
+                return "No more bottles of beer on the wall, " .
+                    "no more bottles of beer.\n" .
+                    "Go to the store and buy some more, " .
                     "99 bottles of beer on the wall.\n";
-                break;
-
             case 1:
-                return
-                    "1 bottle of beer on the wall, ".
-                    "1 bottle of beer.\n".
-                    "Take it down and pass it around, ".
+                return "1 bottle of beer on the wall, " .
+                    "1 bottle of beer.\n" .
+                    "Take it down and pass it around, " .
                     "no more bottles of beer on the wall.\n";
-                break;
-
             case 2:
-                return
-                    "2 bottles of beer on the wall, ".
-                    "2 bottles of beer.\n".
-                    "Take one down and pass it around, ".
+                return "2 bottles of beer on the wall, " .
+                    "2 bottles of beer.\n" .
+                    "Take one down and pass it around, " .
                     "1 bottle of beer on the wall.\n";
-                break;
-
             default:
-                return
-                    "$number bottles of beer on the wall, ".
-                    "$number bottles of beer.\n".
-                    "Take one down and pass it around, ".
-                    ($number - 1)." bottles of beer on the wall.\n";
+                return "{$number} bottles of beer on the wall, " .
+                    "{$number} bottles of beer.\n" .
+                    "Take one down and pass it around, " .
+                    ($number - 1) . " bottles of beer on the wall.\n";
         }
     }
 }
