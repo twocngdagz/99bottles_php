9904225c2dfd3455a64a6015f64511c2d67b1f17
David Stockton <dave@davidstockton.com>
2018-07-28 23:35:06 -0600

    Improve code line breaks for book layout

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -17,9 +17,13 @@ class Bottles
     public function verse($n)
     {
         return
-            ($n === 0 ? 'No more' : $n). ' bottle' .($n !== 1 ? 's' : ''). ' of beer on the wall, ' .
-            ($n === 0 ? 'no more' : $n). ' bottle' .($n !== 1 ? 's' : '')." of beer.\n".
-            ($n > 0 ? 'Take ' .($n > 1 ? 'one' : 'it'). ' down and pass it around, ' : 'Go to the store and buy some more, ').
-            ($n - 1 < 0 ? 99 : ($n - 1 === 0 ? 'no more' : $n - 1)). ' bottle' .($n - 1 !== 1 ? 's' : '')." of beer on the wall.\n";
+            ($n === 0 ? 'No more' : $n) . ' bottle' .($n !== 1 ? 's' : '') .
+                ' of beer on the wall, ' .
+            ($n === 0 ? 'no more' : $n) . ' bottle' .($n !== 1 ? 's' : '') .
+                " of beer.\n".
+            ($n > 0 ? 'Take ' .($n > 1 ? 'one' : 'it') .
+                ' down and pass it around, ' : 'Go to the store and buy some more, ').
+            ($n - 1 < 0 ? 99 : ($n - 1 === 0 ? 'no more' : $n - 1)) .
+                ' bottle' . ($n - 1 !== 1 ? 's' : '')." of beer on the wall.\n";
     }
 }
