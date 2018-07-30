ba8450e832ee1bfcf4844d42241210874d26f3ff
David Stockton <dave@davidstockton.com>
2018-07-29 21:01:15 -0600

    Fix word wrap and formatting for tdd-03-code-b

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -7,6 +7,7 @@ class Bottles
         return "{$number} bottles of beer on the wall, " .
             "{$number} bottles of beer.\n" .
             "Take one down and pass it around, " .
-            ($number - 1) . " bottle" . ($number-1 === 1? '': 's')  . " of beer on the wall.\n";
+            ($number - 1) . " bottle" . ($number - 1 === 1? '': 's')  .
+            " of beer on the wall.\n";
     }
 }
