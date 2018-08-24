8f966f2dea91b29a7daa464ca5cfbb77cb373cd3
David Stockton <dave@davidstockton.com>
2018-08-24 00:10:28 -0600

    tdd-06-code-a

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -29,4 +29,17 @@ class Bottles {
           ($number - 1) . " bottles of beer on the wall.\n";
     }
   }
+
+  public function verses($_, $__) {
+    return
+      "99 bottles of beer on the wall, " .
+      "99 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of beer on the wall.\n" .
+      "\n" .
+      "98 bottles of beer on the wall, " .
+      "98 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "97 bottles of beer on the wall.\n";
+  }
 }
