f5df597bc308d9e9a71955038463463a9b707b83
David Stockton <dave@davidstockton.com>
2018-08-23 23:44:49 -0600

    tdd-01-code

---
+++ lib/Bottles.php
@@ -0,0 +1,11 @@
+<?php
+
+class Bottles {
+  public function verse($_) {
+    return
+      "99 bottles of beer on the wall, " .
+      "99 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of beer on the wall.\n";
+  }
+}
