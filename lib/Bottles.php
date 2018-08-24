ce904a960ce8d8eefb8f21cf4c23d78b60c9f263
David Stockton <dave@davidstockton.com>
2018-08-23 23:50:34 -0600

    tdd-02-code-b

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -2,18 +2,10 @@
 
 class Bottles {
   public function verse($number) {
-    if ($number === 99) {
-      return
-        "99 bottles of beer on the wall, " .
-        "99 bottles of beer.\n" .
-        "Take one down and pass it around, " .
-        "98 bottles of beer on the wall.\n";
-    } else {
-      return
-        "3 bottles of beer on the wall, " .
-        "3 bottles of beer.\n" .
-        "Take one down and pass it around, " .
-        "2 bottles of beer on the wall.\n";
-    }
+    return
+      "{$number} bottles of beer on the wall, " .
+      "{$number} bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      ($number - 1) . " bottles of beer on the wall.\n";
   }
 }
