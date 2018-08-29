4972c65bf1eb678732dca193948cfb7308a62e7a
David Stockton <dave@davidstockton.com>
2018-08-28 23:44:35 -0600

    unearth-00a

--- test/BottlesTest.php
+++ test/BottlesTest.php
@@ -3,8 +3,66 @@
 require __DIR__ . "/../lib/Bottles.php";
 
 class BottlesTest extends \PHPUnit\Framework\TestCase {
+  public function test_the_first_verse() {
+    $expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" .
+      "Take one down and pass it around, 98 bottles of beer on the wall.\n";
+
+    $this->assertEquals($expected, (new Bottles())->verse(99));
+  }
+
+  public function test_another_verse() {
+    $expected = "3 bottles of beer on the wall, 3 bottles of beer.\n" .
+      "Take one down and pass it around, 2 bottles of beer on the wall.\n";
+
+    $this->assertEquals($expected, (new Bottles())->verse(3));
+  }
+
+  public function test_verse_2() {
+    $expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" .
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n";
+
+    $this->assertEquals($expected, (new Bottles())->verse(2));
+  }
+
+  public function test_verse_1() {
+    $expected = "1 bottle of beer on the wall, 1 bottle of beer.\n" .
+      "Take it down and pass it around, no more bottles of beer on the wall.\n";
+
+    $this->assertEquals($expected, (new Bottles())->verse(1));
+  }
+
+  public function test_verse_0() {
+    $expected = "No more bottles of beer on the wall, no more bottles of beer.\n" .
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+
+    $this->assertEquals($expected, (new Bottles())->verse(0));
+  }
+
+  public function test_a_couple_verses() {
+    $expected = "99 bottles of beer on the wall, 99 bottles of beer.\n" .
+      "Take one down and pass it around, 98 bottles of beer on the wall.\n" .
+      "\n" .
+      "98 bottles of beer on the wall, 98 bottles of beer.\n" .
+      "Take one down and pass it around, 97 bottles of beer on the wall.\n";
+
+    $this->assertEquals($expected, (new Bottles())->verses(99, 98));
+  }
+
+  public function test_a_few_verses() {
+    $expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" .
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n" .
+      "\n" .
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" .
+      "Take it down and pass it around, no more bottles of beer on the wall.\n" .
+      "\n" .
+      "No more bottles of beer on the wall, no more bottles of beer.\n" .
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+
+    $this->assertEquals($expected, (new Bottles())->verses(2, 0));
+  }
+
   public function test_the_whole_song() {
-    $expected = <<< SONG
+    $expected = <<<SONG
 99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 
@@ -282,9 +340,9 @@ Take one down and pass it around, 8 bottles of beer on the wall.
 Take one down and pass it around, 7 bottles of beer on the wall.
 
 7 bottles of beer on the wall, 7 bottles of beer.
-Take one down and pass it around, 6 bottles of beer on the wall.
+Take one down and pass it around, 1 six-pack of beer on the wall.
 
-6 bottles of beer on the wall, 6 bottles of beer.
+1 six-pack of beer on the wall, 1 six-pack of beer.
 Take one down and pass it around, 5 bottles of beer on the wall.
 
 5 bottles of beer on the wall, 5 bottles of beer.
@@ -306,7 +364,6 @@ No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
 
 SONG;
-
     $this->assertEquals($expected, (new Bottles())->song());
   }
 }
