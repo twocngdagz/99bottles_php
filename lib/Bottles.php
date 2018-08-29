68d5fd590935dfa8873f5c5f37ce11b8310d1531
David Stockton <dave@davidstockton.com>
2018-08-29 00:11:06 -0600

    unearth-02-e

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -44,6 +44,10 @@ class Bottles {
   }
 
   private function container($number = 'FIXME') {
-    return "bottles";
+    if ($number === 1) {
+      return "bottle";
+    } else {
+      return "bottles";
+    }
   }
 }
