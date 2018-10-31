f0bfe845e775737f9524432257bc378f7280c16f
Dann Stockton <dannstockton@i3logix.com>
2018-10-30 21:39:06 -0600

    b-extract-class-21

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -17,10 +17,11 @@ class Bottles {
   public function verse($number) {
     $bottleNumber = new BottleNumber($number);
     return
-      ucfirst($this->quantity($number)) .
-      " {$this->container($number)} of beer on the wall, " .
-      "{$this->quantity($number)} {$this->container($number)} of beer.\n" .
-      "{$this->action($number)}, " .
+      ucfirst($bottleNumber->quantity()) .
+      " {$bottleNumber->container()} of beer on the wall, " .
+      "{$bottleNumber->quantity()} " .
+      "{$bottleNumber->container()} of beer.\n" .
+      "{$bottleNumber->action()}, " .
       "{$this->quantity($this->successor($number))} " .
       "{$this->container($this->successor($number))} " .
       "of beer on the wall.\n";
